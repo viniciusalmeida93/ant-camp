67637199c322336ce5c46cf170b65f8f90a58869
User <user@example.com>
2025-11-26 22:38:13 -0300

    Remove landing page - redirect root to login

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/layout/Navbar";
-import LandingPage from "./pages/LandingPage";
 import Auth from "./pages/Auth";
 import OrganizerDashboard from "./pages/OrganizerDashboard";
 import PublicRegistration from "./pages/PublicRegistration";
@@ -45,7 +44,7 @@ const App = () => (
         <BrowserRouter>
         <Routes>
           {/* Public routes without navbar */}
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<Auth />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/setup" element={<Setup />} />
           <Route path="/integrations" element={<Integrations />} />
