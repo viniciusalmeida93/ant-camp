5fb0064c8e9ea19ede845132091f98e0b40c204b
User <user@example.com>
2025-12-03 13:40:21 -0300

    refactor: aumentar levemente arredondamento para 5px

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -8,7 +8,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(({ classNa
   return (
     <textarea
         className={cn(
-          "flex min-h-[100px] w-full rounded-[3px] border border-input bg-background px-4 py-3 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[100px] w-full rounded-[5px] border border-input bg-background px-4 py-3 text-base ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
       ref={ref}
